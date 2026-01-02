007edb95f8cb6bc6d52182d6fc13458c0f2cb3d2
Sava Kujundzic <87948924+hvlsavo@users.noreply.github.com>
2026-01-02 19:24:12 +0100

    added sounds, sprites for the two phases.

--- Characters/NPCS/ucenik1/ucenik_1.gd
+++ Characters/NPCS/ucenik1/ucenik_1.gd
@@ -11,19 +11,38 @@ signal start_battle(ucenik)
 @onready var sprite_phase2 = $Phase2
 @onready var sprite_phase3 = $Phase3
 
+@onready var alert_sprite = $AlertedAnim
+@onready var alert_sound = $AlertedAnim/Alert
+@onready var phase1_anim = $Phase1Anim
+@onready var phase2_anim = $Phase2Anim
+
+
+@onready var transition = $Transition
+var transition_sound_ended : bool = false
 func _ready():
 	sprite_phase1.texture = phase_1
 	sprite_phase2.texture = phase_2
 	sprite_phase3.texture = phase_3
 	add_to_group("ucenici")
 
+	alert_sprite.modulate.a = 0.0
+
+func _physics_process(delta: float) -> void:
+	phase1_anim.play("idle")
+
 func _on_area_2d_body_entered(body: CharacterBody2D) -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property(alert_sprite, "modulate:a", 1.0, 0.3).set_trans(Tween.TRANS_SINE)
+	tween.tween_property(alert_sprite, "modulate:a", 0.0, 0.3).set_trans(Tween.TRANS_SINE)
+	alert_sound.play()
 	start_battle.emit(self)
 
 # handleri koje battle poziva SAMO za ovog učenika
 func _on_battle_half_monster_hp_left():
+	transition.play()
 	sprite_phase1.hide()
 	sprite_phase2.show()
+	phase2_anim.play("idle")
 
 func _on_battle_player_won():
 	sprite_phase2.hide()
